1e80ecd8a1be9a99320899e7088238122c05aed2
Fernanda Andrade <fernandaao@gmail.com>
2021-07-17 12:46:47 -0300

    adding configurations for the devise confirmable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_03_143841) do
+ActiveRecord::Schema.define(version: 2021_07_17_141646) do
 
   create_table "items", force: :cascade do |t|
     t.integer "tag_id", null: false
@@ -57,6 +57,10 @@ ActiveRecord::Schema.define(version: 2021_07_03_143841) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
